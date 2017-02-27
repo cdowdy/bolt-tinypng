5b47827cfab4db2ca6f76ad64b7dc2002c8c72af
cdowdy <cdowdy46@gmail.com>
2017-02-27 14:21:14 -0500

    adds javascript and ajax functions to send to tinypng

--- web/tinypng.optimize.js
+++ web/tinypng.optimize.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 
-    var tinyPNGOptimize = $(".tinyPNG-optimize");
+    // var tinyPNGOptimize = $(".tinyPNG-optimize");
+    var tinyPNGOptimize = $(".tinyPNG-buttons");
 
 
     tinyPNGOptimize.on({
@@ -8,86 +9,74 @@ $(document).ready(function () {
         click: function (event) {
             var target = event.target;
             var img = target.getAttribute('data-imagePath');
-            // var configData = $(target).parent().prev().find('.prime-cache-dropdown').val();
-            // var singleData = $(target).parent().prev().find('.prime-cache-single-input').val();
-            // var presetData = target.getAttribute('data-presets');
+            var preserveData = target.getAttribute('data-optiparam');
 
-            prime(target, img);
-            // if (target.classList.contains('prime-config')) {
-            //     prime(target, img, configData, 'config');
-            // }
-            //
-            // if (target.classList.contains('prime-presets')) {
-            //     prime(target, img, presetData, 'presets');
-            // }
-            //
-            // if (target.classList.contains('prime-single')) {
-            //     if( singleData === "" ) {
-            //         $(target).parent().prev().find(".prime-single-error").removeAttr("hidden");
-            //         return false;
-            //     } else {
-            //         console.log(singleData);
-            //         prime(target, img, singleData, 'single');
-            //     }
-            //
-            // }
+            if (target.classList.contains('tinyPNG-optimize')) {
+                event.preventDefault();
+                // console.log("the image: " + img + " data to preserve: " + preserveData );
+                optimize(img, preserveData);
+            }
+
+            if (target.classList.contains('tnypng-copy')) {
+                event.preventDefault();
+                // console.log("the image: " + img + " data to preserve: " + preserveData );
+                optimize(img, preserveData);
+            }
+
+            if (target.classList.contains('tnypng-create')) {
+                event.preventDefault();
+                optimize(img, preserveData);
+            }
+
+            if (target.classList.contains('tnypng-location')) {
+                event.preventDefault();
+                optimize(img, preserveData);
+            }
+
+            if ( target.classList.contains('tnypng-allthree')) {
+                event.preventDefault();
+                optimize(img, preserveData);
+            }
+
+            // This is the modal window to optimize and rename an image
+            if (target.classList.contains('tnypng-submit')) {
+                var form = $(target).closest('.form-horizontal');
+                var theName = form.find("input[type=text]").val();
+                var preserveRadio = form.find("input[name=preserveOption]:checked").val();
+                event.preventDefault();
+
+                if (theName === '') {
+                    form.find(".control-label").parent().addClass("has-error");
+                    form.find(".control-label").attr("aria-invalid", true);
+                    form.find("input[type=text]").siblings(".input-error").attr("hidden", false);
+                } else {
+                    optimizeRename(img, theName, preserveRadio);
+                }
+            }
         },
 
-        // change: function (event) {
-        //     var target = event.target;
-        //     var code = event.keyCode || event.which;
-        //
-        //     if ($(target).hasClass('prime-cache-single-input') && $(".prime-cache-single-input").length > 0 ) {
-        //         $(target).parent().next().attr("hidden", "hidden");
-        //     }
-        // },
+        change: function (event) {
+            var target = event.target;
+            var form = $(target).closest('.form-horizontal');
 
-
-        // keypress: function (event) {
-        //     var target = event.target;
-        //     var code = event.keyCode || event.which;
-        //     var buttonToClick = $(target).parent().parent().next().find("BUTTON");
-        //     var img = $(target).parent().parent().next().find("BUTTON").data("bthumb-name");
-        //     var modType = $(target).val();
-        //     var presetData = $(target).data('presets');
-        //
-        //     if ( code === 13 ) {
-        //         event.preventDefault();
-        //
-        //         if (buttonToClick.hasClass('prime-config')) {
-        //             $(target).on('click', prime(buttonToClick, img, modType, 'config' ) );
-        //         }
-        //         if ($(target).hasClass('prime-presets') ) {
-        //             $(target).on('click', prime('.prime-presets', img, presetData, 'presets' ) );
-        //         }
-        //
-        //         if (buttonToClick.hasClass('prime-single') ) {
-        //
-        //             if ($(target).val() === "") {
-        //                 $(target).parent().next().removeAttr("hidden");
-        //                 return false;
-        //             } else {
-        //                 $(target).on('click', prime(buttonToClick, img, modType, 'single') );
-        //             }
-        //
-        //         }
-        //
-        //     }
-        // }
-
+            if ($(target).hasClass('new-name-input') && $(".new-name-input").length > 0 ) {
+                form.find(".control-label").parent().removeClass('has-error');
+                form.find("input[type=text]").siblings(".input-error").attr("hidden", true);
+            }
+        }
     });
 
 
-    var prime = function (target, img ) {
+    var optimize = function ( img, preserve ) {
         $.ajax({
             type: "POST",
-            url: $( ".dashboardlisting").data("bolt-path") + 'extend/tinypng/optimize',
+            url: $( ".dashboardlisting").data("bolt-path") + '/extend/tinypng/optimize',
             data: {
-                'image' : img
+                'image' : img,
+                'preserve' : preserve
             },
 
             beforeSend: function () {
-                console.log(url);
                 disableButtons();
 
             },
@@ -99,8 +88,44 @@ $(document).ready(function () {
             error: function (xhr, desc, err) {
                 console.log(xhr);
                 console.log("Details: " + desc + "\nError:" + err);
+                $(".ajax-error").append(xhr.responseText);
             }
         });
+    };
+
+    var optimizeRename = function (img, newName, preserve ) {
+        $.ajax({
+            type: "POST",
+            url: $( ".dashboardlisting").data("bolt-path") + '/extend/tinypng/optimize/rename',
+            data: {
+                'image' : img,
+                'newName' : newName,
+                'preserve' : preserve
+            },
+
+            beforeSend: function () {
+                disableButtons();
+
+            },
+
+            success: function () {
+                enableButtons();
+            },
+
+            error: function (xhr, desc, err) {
+                console.log(xhr);
+                console.log("Details: " + desc + "\nError:" + err);
+                $(".ajax-error").append(xhr.responseText);
+            }
+        });
+
+    };
+
+    var verifyNewName = function(newName) {
+        if (newName === '') {
+            $(newName).parent().prev("LABEL").addClass(".has-error");
+            $(newName).parent().prev("LABEL").attr("aria-invalid", true);
+        }
     };
 
 
