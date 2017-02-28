0aa49055501f4865f9ec5344168a2df60677f700
cdowdy <cdowdy46@gmail.com>
2017-02-28 09:52:01 -0500

    code cleanup for main extension file

--- src/TinyPNGExtension.php
+++ src/TinyPNGExtension.php
@@ -3,60 +3,54 @@
 namespace Bolt\Extension\cdowdy\tinypng;
 
 $autoload = __DIR__ . '/../vendor/autoload.php';
-if (is_file($autoload)) {
+if ( is_file( $autoload ) ) {
 	require $autoload;
 }
 
 
 use Bolt\Asset\File\JavaScript;
-use Bolt\Asset\File\Stylesheet;
+
 use Bolt\Controller\Zone;
 
 use Bolt\Extension\cdowdy\tinypng\Controller\TinyPNGBackendController;
 use Bolt\Extension\SimpleExtension;
 use Bolt\Menu\MenuEntry;
-use Tinify;
+
 
 /**
  * ExtensionName extension class.
  *
  * @author Your Name <you@example.com>
  */
-class TinyPNGExtension extends SimpleExtension
-{
+class TinyPNGExtension extends SimpleExtension {
 
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function registerAssets()
-    {
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function registerAssets() {
 
 
-	    $asset = new JavaScript();
-	    $asset->setFileName('/extensions/vendor/cdowdy/tinypng/tinypng.optimize.js')
-	          ->setLate(true)
-	          ->setPriority(99)
-	          ->setAttributes(['defer', 'async'])
-	          ->setZone(Zone::BACKEND)
-	    ;
+		$asset = new JavaScript();
+		$asset->setFileName( '/extensions/vendor/cdowdy/tinypng/tinypng.optimize.js' )
+		      ->setLate( true )
+		      ->setPriority( 99 )
+		      ->setAttributes( [ 'defer', 'async' ] )
+		      ->setZone( Zone::BACKEND );
 
 
-	    return [ $asset];
-    }
-
+		return [ $asset ];
+	}
 
 
 	/**
 	 * @return array
 	 */
-	protected function registerMenuEntries()
-	{
-		$menu = new MenuEntry('tinypng-menu', 'tinypng');
-		$menu->setLabel('TinyPNG Image Optimization')
-		     ->setIcon('fa:image')
-		     ->setPermission('settings')
-		;
+	protected function registerMenuEntries() {
+		$menu = new MenuEntry( 'tinypng-menu', 'tinypng' );
+		$menu->setLabel( 'TinyPNG Image Optimization' )
+		     ->setIcon( 'fa:image' )
+		     ->setPermission( 'settings' );
 
 		return [
 			$menu,
@@ -67,20 +61,18 @@ class TinyPNGExtension extends SimpleExtension
 	 * @return array
 	 * backend controller for optimization and info  page
 	 */
-	protected function registerBackendControllers()
-	{
+	protected function registerBackendControllers() {
 		$config = $this->getConfig();
 
 		return [
-			'/extend/tinypng' => new TinyPNGBackendController($config),
+			'/extend/tinypng' => new TinyPNGBackendController( $config ),
 		];
 	}
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function registerTwigPaths()
-    {
-        return ['templates'];
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function registerTwigPaths() {
+		return [ 'templates' ];
+	}
 }
