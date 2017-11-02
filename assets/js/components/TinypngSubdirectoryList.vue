11e35bbacc3b1bccf03b315b3729a5bf0d061624
Cory <cory@corydowdy.com>
2017-11-02 10:40:39 -0400

    add initial sub-directory list vuejs component

---
+++ assets/js/components/TinypngSubdirectoryList.vue
@@ -0,0 +1,26 @@
+<template>
+    <p>
+        <a :href="subDirectoryPath">
+            {{ subdirectory }}
+        </a>
+    </p>
+</template>
+
+<script>
+    export default {
+
+        name: 'tinypng-subdirectory',
+
+        props: {
+
+            subdirectory: {
+
+            },
+
+            subDirectoryPath: {
+                type: String,
+            }
+        },
+
+    }
+</script>
