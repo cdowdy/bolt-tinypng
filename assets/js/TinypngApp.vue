55868074fab589c9f823279193004b0c343db931
Cory <cory@corydowdy.com>
2017-12-13 11:30:53 -0500

    updates main tinypngapp.vue

--- assets/js/TinypngApp.vue
+++ assets/js/TinypngApp.vue
@@ -15,13 +15,13 @@
 
         <div class="col-xs-12 col-md-5">
 
-                <alert type="success" v-for="alert in uploadedAlerts" :key="alert.key"
-                       v-show="showNotification"
-                       :dismissible="true"
-                       @dismissed="showNotification = false"
-                       :duration="duration">
-                    {{ uploaded }} has been uploaded!
-                </alert>
+            <alert type="success" v-for="(index, alert) in uploadedAlerts" :key="alert.key"
+                   v-show="showNotification"
+                   :dismissible="true"
+                   @dismissed="uploadedAlerts.splice(index, 1)"
+                   :duration="duration">
+                {{ index.key }} has been uploaded!
+            </alert>
 
 
         </div>
@@ -38,10 +38,10 @@
                 <tbody>
                 <tr>
                     <td id="tnypng-upload-form-con      ">
-                        <Tinypng-Dropzone  @tnydropzone-success="pushUploadedFile" :acceptedFiles="''"
-                                id="tinypng_upload_form"
-                                :url="dropzoneUrl"
-                                >
+                        <Tinypng-Dropzone @tnydropzone-success="pushUploadedFile"
+                                          id="tinypng_upload_form"
+                                          :url="dropzoneUrl"
+                        >
                             <fieldset id="form_tinypng">
                                 <div class="fallback">
                                     <div class="form-group">
@@ -57,7 +57,8 @@
                                     </div>
                                     <div class="form-group">
                                         <button type="submit"
-                                                class="btn btn-primary tinypng-upload">Upload File</button>
+                                                class="btn btn-primary tinypng-upload">Upload File
+                                        </button>
                                     </div>
                                 </div>
                             </fieldset>
@@ -66,11 +67,11 @@
                     </td>
                     <td>
                         <p><b>Upload Method:</b> {{ uploadMethod }}</p>
-                        <p v-if="maxWidth">
-                            <b>Width:</b> {{ maxWidth }}px
+                        <p v-if="uploadMaxWidth">
+                            <b>Width:</b> {{ uploadMaxWidth }}px
                         </p>
-                        <p v-if="maxHeight">
-                            <b>Height:</b> {{ maxHeight }}px
+                        <p v-if="uploadMaxHeight">
+                            <b>Height:</b> {{ uploadMaxHeight }}px
                         </p>
 
                         <!--{#<p><b>Metadata:</b> {{ saveData }}</p>#}-->
@@ -83,17 +84,21 @@
         </div><!-- /.col-xs-12 upload conatainer -->
         <div class="col-xs-12">
 
-            <tabs >
-                <tab title="Files" selected="true">
+            <tabs>
+                <tab title="Files"
+                     selected="true">
                     <tinypng-filelist
                             :files="fileList"
                             :tinypng-path="tinypngPath"
                             :tinypngDeletePath="tinypngDeletePath"
-                             >
+                            :tinypng-rename-path="tinypngRenamePath"
+                            :tinypng-batch-optimize="tinypngBatchOptimize"
+                            @tinypng-image-optimized="updateTinypngData"
+                            @tinypng-batch-optimized="updateCompressionCount">
                     </tinypng-filelist>
                 </tab>
 
-                <tab title="Directories" >
+                <tab title="Directories">
 
                     <table class="dashboardlisting">
                         <thead>
@@ -108,9 +113,9 @@
                         </tbody>
                     </table>
 
-                    <tinypng-newdir :directory=" directory"
-                                    :create-action-path="createPath "
-                                    :delete-action-path="deletePath"
+                    <tinypng-newdir :directory=" currentDirectory"
+                                    :create-action-path="createDirectoryPath "
+                                    :delete-action-path="deleteDirectoryPath"
                                     :directories="tinypngDirectories"></tinypng-newdir>
                 </tab>
             </tabs>
@@ -120,14 +125,12 @@
 
 <script>
     import TinypngDropzone from './components/TinypngDropzone.vue';
-    // import BoltTabs from './components/Bolt/BoltTabs.vue';
-    // import BoltTabpanel from './components/Bolt/BoltTabsPanels';
     import TinypngFilelist from './components/TinypngFilelisting';
     import TinypngDirectory from './components/TinypngDirectoryList.vue';
     import TinypngSubdirectory from './components/TinypngSubdirectoryList';
     import TinypngNewdir from './components/TinypngCreateDirectory';
 
-    import { Alert, Tab, Tabs } from 'uiv';
+    import {Alert, Tab, Tabs} from 'uiv';
 
     export default {
 
@@ -146,11 +149,11 @@
             files: {
                 required: true
             },
-            maxWidth: {
+            uploadMaxWidth: {
                 type: String,
                 required: true
             },
-            maxHeight: {
+            uploadMaxHeight: {
                 type: String,
                 required: true
             },
@@ -160,17 +163,27 @@
                 required: true
             },
             uploadMethod: {
-              type: String,
-              required: true
+                type: String,
+                required: true
             },
 
             tinypngPath: {
                 type: String,
                 required: true
             },
+
+            tinypngBatchOptimize: {
+                type: String,
+                required: true
+            },
             tinypngDeletePath: {
                 type: String,
                 required: true
+            },
+
+            tinypngRenamePath: {
+                required: true,
+                type: String,
             },
 
             allImagesPath: {
@@ -181,15 +194,16 @@
                 required: true,
             },
 
-            createPath: {
+            createDirectoryPath: {
                 required: true,
             },
 
-            deletePath:{
+            deleteDirectoryPath: {
                 required: true,
             },
 
-            directory: {
+
+            currentDirectory: {
                 required: true
             },
 
@@ -219,58 +233,35 @@
         },
 
         methods: {
-
-            toggleBatch: function () {
-                this.batchVisible = !this.batchVisible;
-            },
-            // push new file to our file list
-            // listen for this event from tinypngdropzone.vue 'tnydropzone-success'
-            // vm.$emit('tnydropzone-success', file, response, xhr )
             pushUploadedFile: function (file, response, xhr) {
 
                 response.forEach(object => {
 
                     this.fileList.push({
                         filename: object.filename,
-                        imageWidth: object.imageWidth,
-                        imageHeight: object.imageHeight,
                         filesize: object.optimizedSize,
-                        located: this.directory,
-                        imagePath: object.name,
+                        imageHeight: object.imageHeight,
+                        imageWidth: object.imageWidth,
+                        imagePath: object.imagePath,
+                        located: this.currentDirectory,
                     });
-                    //
+
                     this.compressions = object.compressionCount;
-                    this.uploadedAlerts.push({ key: object.filename } );
-                    this.uploaded = object.filename;
+                    this.uploadedAlerts.push({key: object.filename});
                     this.showNotification = true;
                 });
             },
 
             updateTinypngData: function (response) {
 
-                console.log( response );
-
                 response.forEach(object => {
-
-                    // response.forEach(object => {
-                    //     this.fileList.splice(index, 1, {
-                    //         filename: object.optimizedImage,
-                    //         filesize: object.optimizedSize,
-                    //         imageHeight: object.imageHeight,
-                    //         imageWidth: object.imageWidth,
-                    //         located: this.directory,
-                    //     });
-                    // });
-
                     this.compressions = object.compressionCount;
-
                 });
-
             },
 
-            // deleteFile: function( index ) {
-            //     this.$delete(this.fileList, index);
-            // }
+            updateCompressionCount: function (response) {
+                this.compressions = response;
+            },
         },
 
     }
