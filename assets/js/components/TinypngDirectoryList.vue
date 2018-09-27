dc38fe24ed0414d71bba1e9dcc47da0e9850b4f3
Cory Dowdy <cdowdy46@gmail.com>
2018-09-27 10:43:13 -0400

    adds directory sorting to the directories list

--- assets/js/components/TinypngDirectoryList.vue
+++ assets/js/components/TinypngDirectoryList.vue
@@ -1,28 +1,53 @@
 <template>
-    <tbody>
-    <tr>
-        <td>
-            <a :href="pathAllImages">
-                Files
-            </a>
-        </td>
-        <td></td>
-    </tr>
-    <tr v-for="directory in directoriesList">
-        <td>
-            <a :href="directory.route ">
-                {{ directory.path }}
-            </a>
-        </td>
-        <td>
-            <p v-for="dir in directory.subdirectory" v-if="dir.path">
-                <a :href="dir.route">
-                    {{ dir.path }}
-                </a>
-            </p>
-        </td>
-    </tr>
-    </tbody>
+    <table class="dashboardlisting"
+           role="grid"
+           aria-labelledby="directoryCaption"
+           aria-readonly="true"
+           id="tinypng-dirlist-table"
+    >
+        <caption id="directoryCaption">
+            Image Directories <span v-if="captionVisible">Sorted By Directory: {{ activeSortDirection }}</span>
+        </caption>
+        <thead>
+        <tr role="row">
+            <th scope="col"
+                role="columnheader"
+                :aria-sort=" activeSortDirection ? activeSortDirection : 'none' "
+                class="tinypng-table-header"
+                @click="sortBy('path')"
+            >
+                <span role="button"
+                      tabindex="0"
+                      class="tinypng-table-header--button" >
+                    Directory
+                    <i class="fa"
+                       :class=" sortedIcons ">
+                    </i>
+                </span>
+            </th>
+            <th class="tinypng-table-header"
+                @click="sortBy('subdirectory')">
+                Subdirectory
+            </th>
+        </tr>
+        </thead>
+        <tbody>
+            <tr v-for="directory in sortedDirectories" role="row">
+                <td>
+                    <a :href="directory.route ">
+                        {{ directory.path }}
+                    </a>
+                </td>
+                <td>
+                    <p v-for="dir in directory.subdirectory" v-if="dir.path">
+                        <a :href="dir.route">
+                            {{ dir.path }}
+                        </a>
+                    </p>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </template>
 
 <script>
@@ -37,32 +62,100 @@
             directory: {
 
             },
-            // subdirectory: {
-            //
-            // },
-
             pathAllImages: {
                 type: String,
             },
             subDirectoryPath: {
                 type: String,
-            }
+            },
         },
 
         data() {
+
             return {
-                directoriesList: this.directories
+                captionVisible: false,
+                sortDirection: 'asc',
+
+                sort: {
+                    sortBy: 'path',
+                    activeDirection: '',
+                },
+                sortIcons: {
+                    asc: false,
+                    desc: false
+                },
+
+                directoriesList: this.directories,
+                currentSort: 'path',
             }
         },
+
+        methods: {
+            sortBy: function (sort) {
+                if (sort === this.sort.sortBy) {
+                    this.sort.activeDirection = this.sort.activeDirection === 'ascending' ? 'descending' : 'ascending';
+                    this.captionVisible = true;
+                }
+
+                this.sort.sortBy = sort;
+            }
+        },
+
 
         computed: {
             hasSubDirectory() {
                 return Object.keys(this.directoriesList).length > 0;
+            },
+
+            activeSortDirection(){
+                return this.sort.activeDirection;
+            },
+
+            sortedDirectories() {
+
+                if ( this.sort.activeDirection === 'ascending' ) {
+
+                    this.sortIcons.asc = true;
+                    this.sortIcons.desc = false;
+
+                    return this.directoriesList.sort( (a, b) => {
+                        let pathA = a.path.toLocaleLowerCase();
+                        let pathB = b.path.toLocaleLowerCase();
+
+                        return pathA.localeCompare(pathB);
+                    })
+                }
+
+                if ( this.sort.activeDirection === 'descending' ) {
+
+                    this.sortIcons.asc = false;
+                    this.sortIcons.desc = true;
+
+
+                    return this.directoriesList.sort( (a, b) => {
+                        let pathA = a.path.toLocaleLowerCase();
+                        let pathB = b.path.toLocaleLowerCase();
+
+
+                        return pathB.localeCompare(pathA);
+                    })
+                }
+
+                return this.directoriesList;
+            },
+
+            // should refactor this to use props or data instead of returning the
+            // icon name in a string
+            sortedIcons() {
+                if ( this.sort.activeDirection === 'ascending' ) {
+                    return 'fa-arrow-down';
+                }
+                if ( this.sort.activeDirection === 'descending' ) {
+                    return 'fa-arrow-up';
+                }
+                return 'fa-arrows-v';
             }
+
         },
-        //
-        // mounted() {
-        //     this.isActive = this.selected;
-        // }
     }
 </script>
