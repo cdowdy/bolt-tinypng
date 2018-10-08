87f87ffea37074a4cc46a8c8c6d1167992683dd7
Cory Dowdy <cdowdy46@gmail.com>
2018-10-08 10:38:38 -0400

    adds directory search by name of directory

--- assets/js/components/TinypngDirectoryList.vue
+++ assets/js/components/TinypngDirectoryList.vue
@@ -18,7 +18,7 @@
             >
                 <span role="button"
                       tabindex="0"
-                      class="tinypng-table-header--button" >
+                      class="tinypng-table-header--button">
                     Directory
                     <i class="fa"
                        :class=" sortedIcons ">
@@ -31,37 +31,69 @@
                 Subdirectory
             </th>
         </tr>
+        <!-- TODO abstract this into its own component because this is getting "large" -->
+        <tr role="row">
+            <th scope="col"
+                role="columnheader">
+                <form action="">
+                    <div class="form-group ">
+                        <label for="directorySearch"
+                               class="control-label">
+                            Search By Directory Name:
+                        </label>
+                        <input type="search"
+                               id="directorySearch"
+                               class="form-control"
+                               aria-describedby="DirectorySearchDescription"
+                               placeholder="Search For Directory Name"
+                               v-model="filteredDirectories">
+                        <span class="sr-only"
+                              id="DirectorySearchDescription">Search for Directory</span>
+                    </div>
+                </form>
+            </th>
+            <th>
+                &nbsp;
+            </th>
+        </tr>
         </thead>
         <tbody>
-           <template v-for="directory in sortedDirectories" >
-               <template v-if="directory.subdirectory.length < 1">
-                   <tr role="row">
-                       <td>
-                           <a :href="directory.route ">
-                               {{ directory.path }}
-                           </a>
-                       </td>
-                       <td></td>
-                   </tr>
-               </template>
-               <template v-else>
-                   <tr role="row"
-                       :rowspan="directory.subdirectory.length">
-                       <td>
-                           <a :href="directory.route ">
-                               {{ directory.path }}
-                           </a>
-                       </td>
-                       <td>
-                           <p v-for="dir in directory.subdirectory" v-if="dir.path">
-                               <a :href="dir.route">
-                                   {{ dir.path }}
-                               </a>
-                           </p>
-                       </td>
-                   </tr>
-               </template>
-           </template>
+        <template v-if="sortedDirectories.length < 1 ">
+            <tr>
+                <td>
+                    No Directory Here
+                </td>
+            </tr>
+        </template> <!-- ./ length less than 1 -->
+        <template v-for="directory in sortedDirectories">
+            <template v-if="directory.subdirectory.length < 1">
+                <tr role="row">
+                    <td>
+                        <a :href="directory.route ">
+                            {{ directory.path }}
+                        </a>
+                    </td>
+                    <td></td>
+                </tr>
+            </template>  <!-- ./ subdirectory greater than 1 -->
+            <template v-else>
+                <tr role="row"
+                    :rowspan="directory.subdirectory.length">
+                    <td>
+                        <a :href="directory.route ">
+                            {{ directory.path }}
+                        </a>
+                    </td>
+                    <td>
+                        <p v-for="dir in directory.subdirectory" v-if="dir.path">
+                            <a :href="dir.route">
+                                {{ dir.path }}
+                            </a>
+                        </p>
+                    </td>
+                </tr>
+            </template>
+        </template> <!-- ./ for directory in sortedDirectories -->
         </tbody>
     </table>
 </template>
@@ -94,6 +126,8 @@
 
             return {
                 captionVisible: false,
+
+                filteredDirectories:    '',
 
                 sort: {
                     sortBy: 'path',
@@ -133,14 +167,19 @@
             sortedDirectories() {
 
                 let direction = 'asc';
+                // this is for the text based search for filtering by name
+                let  dirfilter = new RegExp(this.filteredDirectories, 'i');
 
                 if (this.sort.activeDirection === 'descending' ) {
                     direction = 'desc';
                 }
 
-                return orderBy( this.directoriesList, [ this.sort.sortBy ], [ direction ] );
+                let directoryList = this.directoriesList.filter( el => el.path.match(dirfilter));
+
+                return orderBy( directoryList, [ this.sort.sortBy ], [ direction ] );
 
             },
+
 
             // should refactor this to use props or data instead of returning the
             // icon name in a string
